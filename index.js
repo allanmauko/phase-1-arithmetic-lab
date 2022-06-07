202c6e27234c30ef2d32c72ad57f80a33a48160a
allanmauko <allan.mauko@student.moringaschool.com>
2022-06-07 10:13:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply=62
+const num1 = 31;
+const num2 = 2;
+multiply=(num1 * num2)
+
+const random= Math.floor(Math.random() *10) +1;
+
+let mod=4
+const num3 = 8;
+const num4 = 2;
+mod=(num3 / num4)
+
+let max=20
+max(10,15,20);
